73b0acbc41d5c07ed886526f65f3d240c5525727
Mohammed Haseeb <mdhaseeb.98@gmail.com>
2023-07-07 22:50:51 +0530

    My first commit from terminal

---
+++ 010_invertBT.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == NULL) {
+            return NULL;
+        }
+
+        TreeNode* temp = root -> left;
+        root -> left = root -> right;
+        root -> right = temp;
+
+        invertTree(root -> left);
+        invertTree(root -> right);
+
+        return root;
+    }
+};
